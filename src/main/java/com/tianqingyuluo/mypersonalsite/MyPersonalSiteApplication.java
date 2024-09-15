d72445e7dd5277e99f7be10019a1730023c98680
tianqingyuluo <chengwanhao2017@163.com>
2024-09-15 20:10:56 +0800

    init

---
+++ src/main/java/com/tianqingyuluo/mypersonalsite/MyPersonalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tianqingyuluo.mypersonalsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonalSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tianqingyuluo/mypersonalsite/MyPersonalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianqingyuluo.mypersonalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
